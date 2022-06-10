cd819dca37b1117f4c369c572d875e68702f15db
meklis <max.boyar.a@gmail.com>
2022-06-10 23:56:55 +0300

    Added huawei

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -15,6 +15,7 @@ class Helpers
         'linux' => Linux::class,
         'xos' => Xos::class,
         'zte' => ZTE::class,
+        'huawei' => Huawei::class,
     ];
 
     /**
---
+++ src/Helpers/Huawei.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Meklis\Network\Console\Helpers;
+
+class Huawei extends DefaultHelper
+{
+    protected $prompt = '<.*?>';
+    protected $userPrompt = 'ame:';
+    protected $passwordPrompt = 'ord:';
+    protected $afterLoginCommands = [
+        'screen-length 0 temporary'
+    ];
+    protected $beforeLogoutCommands = [];
+    protected $eol = "\r\n";
+    protected $doubleLoginPrompt = false;
+    protected $enableMagicControl = false;
+    protected $windowSize = [
+        1024,
+        500
+    ];
+}
