1229953971634d43d8e4de0545a2262981e19617
meklis <max.boyar.a@gmail.com>
2022-07-05 23:22:07 +0300

    Fixed stels

--- src/Helpers/Stels.php
+++ src/Helpers/Stels.php
@@ -4,12 +4,15 @@ namespace Meklis\Network\Console\Helpers;
 
 class Stels extends DefaultHelper
 {
-    protected $prompt = '([A-z_0-9]{1,30})[>#]';
+    protected $prompt = '([A-z_0-9]{1,30})(\(.*\))?[>#]';
     protected $userPrompt = 'Username:';
     protected $passwordPrompt = 'Password:';
     protected $afterLoginCommands = [];
-    protected $beforeLogoutCommands = [];
+    protected $beforeLogoutCommands = [
+        'logo'
+    ];
     protected $windowSize = null;
+    protected $eol = "\r\n";
     protected $paginationDetect = '/--More.*--/';
 
     public function isDoubleLoginPrompt(): bool
