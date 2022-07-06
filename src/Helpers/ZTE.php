42abb25bb2eff8e39602b0ba08892381040b2fbb
meklis <max.boyar.a@gmail.com>
2022-07-06 16:52:52 +0300

    Fixed for ZTE

--- src/Helpers/ZTE.php
+++ src/Helpers/ZTE.php
@@ -10,7 +10,10 @@ class ZTE extends DefaultHelper
     protected $afterLoginCommands = [
         'terminal length 0'
     ];
-    protected $beforeLogoutCommands = [];
+    protected $beforeLogoutCommands = [
+        ['command'=>'exit','no_wait'=>true,'usleep'=>1000],
+        ['command'=>'yes','no_wait'=>true,'usleep'=>1000],
+    ];
     protected $doubleLoginPrompt = false;
     protected $enableMagicControl = true;
     protected $windowSize = [
