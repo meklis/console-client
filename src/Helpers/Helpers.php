13955ef777d258f15825f76232ae4ded0d373fbb
meklis <max.boyar.a@gmail.com>
2022-07-11 19:06:32 +0300

    Added new Huawei type MA series

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -17,6 +17,7 @@ class Helpers
         'zte' => ZTE::class,
         'zte_с220' => ZteC220::class,
         'huawei' => Huawei::class,
+        'huawei_ma' => HuaweiMA::class,
         'stels' => Stels::class,
     ];
 
---
+++ src/Helpers/HuaweiMA.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Meklis\Network\Console\Helpers;
+
+class HuaweiMA extends DefaultHelper
+{
+    protected $prompt = '[>#]';
+    protected $userPrompt = 'ame:';
+    protected $passwordPrompt = 'ord:';
+    protected $afterLoginCommands = [
+        "enable",
+        "scroll\r\n\r\n"
+    ];
+    protected $beforeLogoutCommands = [
+        ['command'=>'quit','no_wait'=>true,'usleep'=>1000],
+        ['command'=>'y','no_wait'=>true],
+    ];
+    protected $eol = "\r\n";
+    protected $doubleLoginPrompt = false;
+    protected $enableMagicControl = false;
+    protected $windowSize = [
+        1024,
+        500
+    ];
+}
