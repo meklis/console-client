68904893a317d55cd385f55f1b4108c6678f6893
meklis <max.boyar.a@gmail.com>
2023-11-30 20:59:31 +0200

    examples

--- src/Helpers/Edgecore.php
+++ src/Helpers/Edgecore.php
@@ -4,9 +4,15 @@ namespace Meklis\Network\Console\Helpers;
 
 class Edgecore extends DefaultHelper
 {
-    //protected $prompt = '^[^{}].*?[>#]$';
-    //protected $userPrompt = 'ame:';
-    //protected $passwordPrompt = 'ord:';
-    //protected $eol = "\r\n";
-    //protected $beforeLogoutCommands = ['quit'];
+    protected $prompt = '^.*[$|#]';
+    protected $userPrompt = 'ame:';
+    protected $passwordPrompt = 'ord:';
+    protected $afterLoginCommands = [
+        'screen-length 0 temporary'
+    ];
+    protected $beforeLogoutCommands = [];
+    protected $eol = "\r\n";
+    protected $doubleLoginPrompt = false;
+    protected $enableMagicControl = true;
+    protected $windowSize = null;
 }
