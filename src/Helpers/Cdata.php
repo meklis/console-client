4ce1a4575d1597b36128520c350c298a17c2edd2
meklis <max.boyar.a@gmail.com>
2025-01-31 22:43:55 +0200

    Fix C-Data

--- src/Helpers/Cdata.php
+++ src/Helpers/Cdata.php
@@ -4,14 +4,14 @@ namespace Meklis\Network\Console\Helpers;
 
 class Cdata extends DefaultHelper
 {
-    protected $prompt = '^(.*?More.*)|([0-9A-Za-z _\-"\(\)]{1,}[>#])$';
+    protected $prompt = '^(.*?More.*)|([0-9A-Za-z _\-"\(\)]{3,}[>#])$';
     protected $userPrompt = 'ame:';
     protected $passwordPrompt = 'assword:';
     protected $afterLoginCommands = [];
     protected $enableMagicControl = false;
     protected $eol = "\n";
     protected $beforeLogoutCommands = [];
-    protected $paginationDetect = '/--More.*--/';
+    protected $paginationDetect = '/--More.*$/';
 
     public function isDoubleLoginPrompt(): bool
     {
